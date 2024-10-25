41fb0cfd1fe223b8088261327b256ae6c382c211
leonardo louie ordonez <leonardolouie30@gmail.com>
2024-10-25 10:52:12 +0800

    feat: adding deployment for infura and verifying it in etherscan

---
+++ README.md
@@ -0,0 +1,49 @@
+# Learn Web3 NFT Project
+
+This project demonstrates how to build and deploy a basic NFT project using Solidity, Truffle, and Ganache.
+
+## Prerequisites
+
+Make sure you have the following installed:
+
+- Node.js
+- Truffle - Framework for smart contract development.
+- Ganache - Personal blockchain for Ethereum development.
+- Infura - Ethereum blockchain node provider.
+- MetaMask - Web3 wallet for interacting with Ethereum.
+
+## Running the Application Locally
+
+You can use Truffle’s built-in development environment to compile, migrate, and test your contracts on a local Ganache instance.
+
+### Start Truffle Development Console
+
+To open the Truffle development console and spin up a local blockchain, run the following command:
+
+```bash
+truffle develop
+```
+
+### Compile the Contracts
+
+Once the console is open, you can compile your contracts by running:
+
+```bash
+truffle compile
+```
+
+## Migrate the Contracts
+
+To deploy your contracts on the local development blockchain, run:
+
+```bash
+truffle migrate
+```
+
+## Running Tests
+
+To run the tests for your contracts, you can use the following command in the Truffle console or your terminal:
+
+```bash
+truffle test
+```
--- truffle-config.js
+++ truffle-config.js
@@ -44,7 +44,11 @@
 // require('dotenv').config();
 // const { MNEMONIC, PROJECT_ID } = process.env;
 
-// const HDWalletProvider = require('@truffle/hdwallet-provider');
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+const fs = require("fs");
+const memonicPhrase = fs.readFileSync(".secrets").toString().trim();
+const infuraProjectId = fs.readFileSync(".infura").toString().trim();
+const etherscanKey = fs.readFileSync(".etherscan").toString().trim();
 
 module.exports = {
   /**
@@ -102,6 +106,18 @@ module.exports = {
     //   network_id: 2111,   // This network is yours, in the cloud.
     //   production: true    // Treats this network as if it was a public net. (default: false)
     // }
+
+    sepolia: {
+      provider: () =>
+        new HDWalletProvider(
+          memonicPhrase,
+          `https://sepolia.infura.io/v3/${infuraProjectId}`
+        ),
+      network_id: 11155111, // Sepolia's id
+      confirmations: 2, // # of confirmations to wait between deployments. (default: 0)
+      timeoutBlocks: 200, // # of blocks before a deployment times out  (minimum/default: 50)
+      skipDryRun: true, // Skip dry run before migrations? (default: false for public nets )
+    },
   },
 
   // Set default mocha options here, use special reporters, etc.
@@ -144,4 +160,8 @@ module.exports = {
   //     }
   //   }
   // }
+  plugins: ["truffle-plugin-verify"],
+  api_keys: {
+    etherscan: etherscanKey,
+  },
 };
